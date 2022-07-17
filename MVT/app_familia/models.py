390491bf3fa47075edf5c5746b3403cda0487228
Juan Ignacio Goicoa <nacho3108@gmail.com>
2022-07-17 16:22:51 -0300

    Creación de app_familia

---
+++ MVT/app_familia/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    edad = models.BigIntegerField()
+    anioNacimiento = models.DateField()
+
--- README.md
+++ README.md
@@ -3,4 +3,5 @@ MVT Coder House
 Desafio entregable clase 18
 
 1er paso: django-admin startproject MVT
+2: Cramos la AppFamilia
 
